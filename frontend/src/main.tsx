1df9dbdff043984b5d7d9b01b77b5fad1082b1e6
Anmol Singh Garg <anmolsinghgarg944@gmail.com>
2025-04-02 08:02:16 +0530

    remove StrictMode wrapper from main application entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
@@ -6,11 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>
 );
